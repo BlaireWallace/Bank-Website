9dac9344662123bda8f2c1b491e8bae9ab5f5a77
DukeTriggah <102817703+DukeTriggah@users.noreply.github.com>
2023-09-16 16:31:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+import ttkbootstrap as ttk
 
 #  -------------------- CONSTANTS --------------------------- #
 YELLOW = "#f7f5dd"
@@ -20,41 +21,49 @@ def create_ID():
     dfdf
 
 # ----------- UI SETUP --------------------- #
-window = tk.Tk()
+window = ttk.Window()
 window.title("Bank Website")
-window.geometry("800x500")
+window.geometry("2000x1500")
 window.resizable(False, False)
 
 
 
 
-canvas = tk.Canvas(width=800, height=500, bg = YELLOW, highlightthickness=0)
+canvas = ttk.Canvas(width=800, height=500, bg = YELLOW, highlightthickness=0)
 
 
 # Creatas the Labels
-menu_title = tk.Label(text= "Sign in and Start Banking", font = (FONT_NAME, 30))
-username_website = tk.Label(text="Username:")
-password_website = tk.Label(text="Password:")
+menu_title = ttk.Label(text= "Sign in and Start Banking", font = (FONT_NAME, 15))
+username_website = ttk.Label(text="Username:")
+password_website = ttk.Label(text="Password:")
 
 
 # Entrys
-entry_username = tk.Entry(width = 21)
-entry_password = tk.Entry(width = 21)
+entry_username = ttk.Entry(width = 21)
+entry_password = ttk.Entry(width = 21)
 
 # Buttons
-sign_up_btn = tk.Button(text="Sign Up", width=10)
+sign_up_btn = ttk.Button(text="Sign Up", width=10)
 
 # Places widgets to the window
-menu_title.grid(row = 10, column = 0)
 
-username_website.grid(row = 0, column = 0)
-entry_username.grid(row = 0, column = 1)
-password_website.grid(row = 1, column = 0)
-entry_password.grid(row = 1, column = 1)
+# menu_title.grid(row = 10, column = 0)
 
-sign_up_btn.grid(row = 4, column = 4)
+# username_website.grid(row = 0, column = 0)
+# entry_username.grid(row = 0, column = 1)
+# password_website.grid(row = 1, column = 0)
+# entry_password.grid(row = 1, column = 1)
+
+# sign_up_btn.grid(row = 4, column = 4)
 
 
-window.rowconfigure(10)
-window.columnconfigure(10)
+# window.rowconfigure(10)
+# window.columnconfigure(10)
+
+menu_title.pack()
+username_website.pack()
+
+
+
+
 window.mainloop()
